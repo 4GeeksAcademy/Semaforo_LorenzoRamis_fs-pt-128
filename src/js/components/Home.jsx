35beb681e78ecca7625c48cf6524e3ea7ee67369
Lorenzo Ramis Marti <159830069+Mr-Encho@users.noreply.github.com>
2025-12-31 12:39:37 +0000

    1.0

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const [activo, setActivo] = useState('roja')
+	const [tieneMorado, setTieneMorado] = useState(false)
+
+	const alternarColor = () => {
+
+		if (tieneMorado) {
+			if (activo === 'morado') {
+				setActivo('verde')
+				return
+			}
+			if (activo === 'verde') {
+				setActivo('amarillo')
+				return
+			}
+			if (activo === 'amarillo') {
+				setActivo('roja')
+				return
+			}
+			if (activo === 'roja') {
+				setActivo('morado')
+				return
+			}
+		}
+
+		if (activo === 'verde') {
+			setActivo('amarillo')
+		}
+		if (activo == 'amarillo') {
+			setActivo('roja')
+		}
+		if (activo == 'roja') {
+			setActivo('verde')
+		}
+	}
+
+	const addMorado = () => {
+
+		if (!tieneMorado) {
+			setTieneMorado(true)
+			setActivo('morado')
+			return
+		}
+		
+		setTieneMorado(false)
+	}
+
+	return (
+		<>
+			<div className="container">
+				<div className={`luz roja ${activo === "roja" ? "activo" : ""}`} onClick={() => setActivo("roja")}></div>
+				<div className={`luz amarillo ${activo === "amarillo" ? "activo" : ""}`} onClick={() => setActivo("amarillo")}></div>
+				<div className={`luz verde ${activo === "verde" ? "activo" : ""}`} onClick={() => setActivo("verde")}></div>
+				{tieneMorado && (
+					<div className={`luz morado ${activo === "morado" ? "activo" : ""}`} onClick={() => setActivo("morado")}></div>
+				)}
+			</div>
+			<div className="d-flex justify-content-evenly">
+				<button type="button" className="btn btn-success" onClick={alternarColor}>Alternar Color</button>
+				<button type="button" className="btn btn-success" onClick={addMorado} >{tieneMorado ? 'Quitar color' : 'Añadir color'}</button>
+			</div>
+		</>
 	);
 };
 
